af0091ec33f6697d288a57326fa5a6e4aa3b93af
Scott Marks <scott@magnolia-heights.com>
2009-03-04 16:09:56 +0000

    Refactoring continues

---
+++ M12/mathieu.h
@@ -0,0 +1,16 @@
+#if !defined(__MATHIEU_H_INCLUDED__)
+#define __MATHIEU_H_INCLUDED__
+/*
+ *  mathieu.h
+ *  Mathieu
+ *
+ *  Created by Scott Marks on 03/04/09.
+ *  Copyright 2009 Magnolia Heights Research and Development.  All rights reserved.
+ *
+ */
+
+#import "m12.h"
+typedef M12Permutation Permutation;
+typedef M12PermutationWithHistory PermutationWithHistory;
+
+#endif /* !defined(__MATHIEU_H_INCLUDED__) */
