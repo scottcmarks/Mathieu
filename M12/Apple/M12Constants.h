7d465002c6e7ec28da1df8144bc4eee96a8f7127
Scott Marks <scott@magnolia-heights.com>
2009-03-10 18:30:17 +0000

    M12 has big balls

---
+++ M12/Apple/M12Constants.h
@@ -0,0 +1,20 @@
+#if !defined(__M12CONSTANTS_H_INCLUDED__)
+#define __M12CONSTANTS_H_INCLUDED__
+
+/*
+ *  M12Constants.h
+ *  SporadicM12
+ *
+ *  Created by Scott Marks on 3/10/09.
+ *  Copyright 2009 Magnolia Heights R&D. All rights reserved.
+ *
+ */
+
+
+// These are fiddly program parameters.
+#define M12BallRadius         30.0
+#define ballFontSize          32.0
+#define tagFontSize           12.0
+#define historyFontSize       13.0
+
+#endif /* !defined(__M12CONSTANTS_H_INCLUDED__) */
