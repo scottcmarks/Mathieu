0ac9a7076c5810829d035d405d0faee6e60b4889
Scott Marks <scott@magnolia-heights.com>
2009-04-27 22:15:27 +0000

    More reorg

--- Apple/Classes/GameModel.h
+++ Apple/Classes/GameModel.h
@@ -6,7 +6,7 @@
 //  Copyright 2009 Magnolia Heights Research and Development. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "Kit.h"
 #import "mathieu.h"
 
 typedef MathieuPermutationWithHistory::PermArray PermArray;
--- Apple/Classes/SporadicMAppDelegate.h
+++ Apple/Classes/SporadicMAppDelegate.h
@@ -6,7 +6,7 @@
 //  Copyright Magnolia Heights Research and Development. 2008. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "Kit.h"
 
 @class RootViewController;
 @class GameModel;
