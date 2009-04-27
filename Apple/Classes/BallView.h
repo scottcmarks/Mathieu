0f55401eff17d71ae2bbef0400f33860892b322f
Scott Marks <scott@magnolia-heights.com>
2009-04-27 22:15:27 +0000

    More reorg

--- Apple/Classes/BallView.h
+++ Apple/Classes/BallView.h
@@ -6,10 +6,10 @@
 //  Copyright 2009 Magnolia Heights Research and Development. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "Kit.h"
 #import "mathieu.h"
 
-@interface BallView : UIView 
+@interface BallView : View 
 {
     int _ballNumber;
 }
